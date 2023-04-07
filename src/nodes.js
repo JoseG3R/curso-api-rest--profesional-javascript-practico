c8ac7200d3b2355706d94aca0f5eac8a56702d0a
Jose <jmu3612@gmail.com>
2023-04-07 11:25:04 -0500

    ultimo commit curso api rest practico

---
+++ src/nodes.js
@@ -0,0 +1,29 @@
+//sections
+const $headerSection = document.querySelector('#header'),
+  $trendingPreviewSection = document.querySelector('#trendingPreview'),
+  $categoriesPreviewSection = document.querySelector('#categoriesPreview'),
+  $genericSection = document.querySelector('#genericList'),
+  $movieDetailSection = document.querySelector('#movieDetail');
+
+//lists & containers
+const $searchForm = document.querySelector('#searchForm'),
+  $trendingMoviesPreviewList = document.querySelector('.trendingPreview-movieList'),
+  $categoriesPreviewList = document.querySelector('.categoriesPreview-list'),
+  $movieDetailCategoriesList = document.querySelector('#movieDetail .categories-list'),
+  $relatedMoviesContainer = document.querySelector('.relatedMovies-scrollContainer');
+
+//elements
+const $headerTitle = document.querySelector('.header-title'),
+  $arrowBtn = document.querySelector('.header-arrow'),
+  $headerCategoryTitle = document.querySelector('.header-title--categoryView');
+
+const $searchFormInput = document.querySelector('#searchForm input'),
+  $searchFormBtn = document.querySelector('#searchBtn');
+
+const $trendingBtn = document.querySelector('.trendingPreview-btn');
+
+const $movieDetailTitle = document.querySelector('.movieDetail-title'),
+  $movieDetailDescription = document.querySelector('.movieDetail-description'),
+  $movieDetailScore = document.querySelector('.movieDetail-score');
+
+
